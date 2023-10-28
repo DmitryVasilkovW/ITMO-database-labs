eda30ac8b795e6fba413f582200f3c8282b9d0d3
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2023-10-28 23:26:46 +0300

    Create Task3.sql

---
+++ Lab3/Task3.sql
@@ -0,0 +1,6 @@
+SELECT pc.Name, COUNT(DISTINCT p.ProductID)
+FROM Production.Product AS p INNER JOIN Production.ProductSubcategory AS psc
+ON p.ProductSubcategoryID = psc.ProductSubcategoryID
+INNER JOIN Production.ProductCategory AS pc
+ON psc.ProductCategoryID = pc.ProductCategoryID
+GROUP BY pc.Name

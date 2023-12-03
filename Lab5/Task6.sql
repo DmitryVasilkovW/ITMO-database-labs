81a402f819cbc5587a4959f3a032ca7339e7276c
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2023-12-03 20:34:14 +0300

    Update Task6.sql

--- Lab5/Task6.sql
+++ Lab5/Task6.sql
@@ -1,11 +1,26 @@
+--WITH CTE AS 
+--(
+--    SELECT CustomerID, ProductID
+--    FROM Sales.SalesOrderHeader soh join Sales.SalesOrderDetail sod
+--    ON soh.SalesOrderID = sod.SalesOrderID
+--    GROUP BY CustomerID, ProductID
+--    HAVING COUNT(DISTINCT sod.SalesOrderID) >= 2
+--)
+
+--SELECT DISTINCT CustomerID
+--FROM CTE;
+
+
 WITH CTE AS 
 (
-    SELECT CustomerID, ProductID
+    SELECT CustomerID, ProductID, COUNT(DISTINCT sod.SalesOrderID) as OrderCount
     FROM Sales.SalesOrderHeader soh join Sales.SalesOrderDetail sod
     ON soh.SalesOrderID = sod.SalesOrderID
     GROUP BY CustomerID, ProductID
-    HAVING COUNT(DISTINCT sod.SalesOrderID) >= 2
 )
 
-SELECT DISTINCT CustomerID
-FROM CTE;
+SELECT CustomerID
+FROM CTE
+WHERE OrderCount >= 2
+GROUP BY CustomerID
+HAVING MIN(OrderCount) >= 2;

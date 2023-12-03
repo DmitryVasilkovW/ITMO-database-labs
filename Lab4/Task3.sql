53d55685e2caf2ea298591b0ef68507c6e6d2044
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2023-12-03 19:57:41 +0300

    Create Task3.sql

---
+++ Lab4/Task3.sql
@@ -0,0 +1,10 @@
+SELECT ProductID
+FROM Sales.SalesOrderDetail
+WHERE ProductID IN 
+  (
+    SELECT sod.ProductID
+    FROM Sales.SalesOrderDetail sod
+    JOIN Sales.SalesOrderHeader soh ON sod.SalesOrderID = soh.SalesOrderID
+    GROUP BY sod.ProductID
+    HAVING COUNT(DISTINCT soh.CustomerID) = 1
+  )
